4bcaaaae6a8e8bdd6d496d5cc0390d144e846ac5
Andrew Huang <andyroiiid@live.com>
2023-03-17 02:28:28 -0500

    Cross Product Benchmarks

--- Benchmarks.cpp
+++ Benchmarks.cpp
@@ -18,6 +18,10 @@ struct Vec {
         float len = sqrt(sum);
         return {x / len, y / len, z / len, w / len};
     }
+
+    [[nodiscard]] Vec Cross(const Vec &v) const {
+        return {y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x, 0.0f};
+    }
 };
 
 TEST_CASE("Normalization Benchmarks") {
@@ -36,6 +40,24 @@ TEST_CASE("Normalization Benchmarks") {
 
     BENCHMARK("SIMD Fast Normalize") {
         volatile SimdVec result = simd.FastNormalize();
+        (void) result;
+    };
+}
+
+TEST_CASE("Cross Product Benchmarks") {
+    const Vec plain{1.0f, 2.0f, 3.0f, 4.0f};
+    const Vec plainB{2.0f, 3.0f, 4.0f, 5.0f};
+
+    BENCHMARK("Plain Cross Product") {
+        volatile Vec result = plain.Cross(plainB);
+        (void) result;
+    };
+
+    const SimdVec simd{1.0f, 2.0f, 3.0f, 4.0f};
+    const SimdVec simdB{2.0f, 3.0f, 4.0f, 5.0f};
+
+    BENCHMARK("SIMD Cross Product") {
+        volatile SimdVec result = simd.Cross(simdB);
         (void) result;
     };
 }
